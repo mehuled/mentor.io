3157b6377cd72923c671f4dce15ba7c0a599559e
Mehul Sharma <mehulcse0209@gmail.com>
2017-10-24 18:37:36 +0530

    The server now accepts basic www.xyz.com/mentor as a valid url

--- mentor/mentor/urls.py
+++ mentor/mentor/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Examples:
+    # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^mentor/', include('mentorapp.urls')), # ADD THIS NEW TUPLE!
 ]
