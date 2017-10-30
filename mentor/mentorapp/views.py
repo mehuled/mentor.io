7af748eaceaa94208e5b9e4234547dc27612e97a
Mehul Sharma <mehulcse0209@gmail.com>
2017-10-31 03:11:09 +0530

    Added links to register Studentand Mentor

--- mentor/mentorapp/views.py
+++ mentor/mentorapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from mentorapp.forms import StudentForm, MentorForm
 
 def index(request):
     context_dict = {'boldmessage' : "I am a bold message"}
@@ -8,3 +9,55 @@ def index(request):
 
 def about(request) :
     return HttpResponse("This is the about page <br/> <a href='/mentor/'>Home</a>")
+
+
+def registerStudent(request) :
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = StudentForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'mentorapp/registerStudent.html', {'form': form})
+
+
+
+
+def registerMentor(request) :
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = MentorForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = MentorForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'mentorapp/registerMentor.html', {'form': form})
